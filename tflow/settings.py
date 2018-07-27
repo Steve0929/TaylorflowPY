bfea5d00dc7d80d5c905da34b3b2204c68f2c6f1
Esteban Jaramillo Sarmiento <chimi2606@hotmail.com>
2018-07-27 18:46:18 -0500

    Update settings.py

--- tflow/settings.py
+++ tflow/settings.py
@@ -117,8 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+'''
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static'),]                         
+'''
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../predictions/static')
