2f9e456110797baad55f5b1d3e6aa4d9d1a112a7
Esteban Jaramillo Sarmiento <chimi2606@hotmail.com>
2018-07-28 11:24:00 -0500

    Update settings.py

--- tflow/settings.py
+++ tflow/settings.py
@@ -123,8 +123,18 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static'),]                         
 '''
-
+'''
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '../predictions/static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, '../predictions/static'))
+'''
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
